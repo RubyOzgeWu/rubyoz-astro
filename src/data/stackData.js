b2a94013b55436b65b0c3ce72b6332b1d013f74f
RubyOzgeWu <rubyozgewu@gmail.com>
2024-09-26 15:36:23 +0800

    feat: stack-view
    - layout

---
+++ src/data/stackData.js
@@ -0,0 +1,31 @@
+export const stacks=[
+  {
+    name:'Vue',
+    iconUrl:'https://vuejs.org/'
+  },
+  {
+    name:'Node.js',
+    iconUrl:'https://vuejs.org/'
+  },
+  {
+    name:'JavaScript',
+    iconUrl:'https://vuejs.org/'
+  },
+  {
+    name:'HTML',
+    iconUrl:'https://vuejs.org/'
+  },
+  {
+    name:'CSS',
+    iconUrl:'https://vuejs.org/'
+  },
+  {
+    name:'Angular',
+    iconUrl:'https://vuejs.org/'
+  },
+  {
+    name:'Blender 3D',
+    iconUrl:'https://vuejs.org/'
+  },
+
+]
